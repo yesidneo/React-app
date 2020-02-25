d26c135951221e4cbd6eed4f02fe7dc49b8a2fd0
Yesid <yesidneo@gmail.com>
2020-02-25 05:03:42 -0300

    change user

--- src/index.js
+++ src/index.js
@@ -9,8 +9,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = <h1>Hello Neo</h1>
+const user = {
+    firstName: 'yam',
+    lastName: 'aguirre',
+    avatar: 'https://www.facebook.com/photo.php?fbid=10204784403915169&set=a.1617812450434&type=3&theater'
+}
 
+function getName(user){
+    return `${user.firstName} ${user.lastName}`
+     
+}
+
+
+function getGreeting(user){
+    if(user){
+        return <h1>Hello {getName(user)} Neo</h1>
+    }
+
+    return <h1>Hello stranger Neo</h1>
+
+
+}
+const name = 'Yesid' 
+  
+const element = <div>{getGreeting(user)}</div>
 const container = document.getElementById('root')
+
 ReactDOM.render(element, container)
 
